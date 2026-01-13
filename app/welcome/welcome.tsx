615600e19c46193df483ae990850b910d5968b7f
osten-antonio <ostenantonio1@gmail.com>
2026-01-13 16:34:08 +0700

    feat: created initial anova page with proper formatting for two way anova

--- app/welcome/welcome.tsx
+++ app/welcome/welcome.tsx
@@ -86,4 +86,27 @@ const resources = [
       </svg>
     ),
   },
+  {
+    href: "/anova",
+    text: "ANOVA Calculator",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="stroke-gray-600 group-hover:stroke-current dark:stroke-gray-300"
+      >
+        <path d="M3 3v18h18" />
+        <path d="M18 17V9" />
+        <path d="M13 17V5" />
+        <path d="M8 17v-3" />
+      </svg>
+    ),
+  },
 ];
